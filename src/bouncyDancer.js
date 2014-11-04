07bb26f152245f09c6f3c35b65204766f007a40a
drabinowitz <dmitrirabinowitz+development@gmail.com>
2014-11-03 23:03:43 -0500

    working on bouncy bouncing towards nearest dancer

--- src/bouncyDancer.js
+++ src/bouncyDancer.js
@@ -2,6 +2,16 @@ var BouncyDancer = function(top, left, timeBetweenSteps){
   Dancer.call(this, top, left, timeBetweenSteps);
   this.$node = $('<span class="bouncy-dancer"></span>');
   this.setPosition(top, left);
+  for(var i = 0; i < window.dancers.length; i++){
+    this.closest = []; 
+    var top = window.dancers[i].css('top');
+    top = +top.substring(0,top.length - 2);
+    var left = window.dancers[i].css('left');
+    left = +left.substring(0,left.length - 2);
+    if(this.getDistance(top,left) < closest[0]){
+      this.closest = [this.getDistance(top, left), top, left];
+    }
+  }
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
 };
